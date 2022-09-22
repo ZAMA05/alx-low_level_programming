fcdc0d5fb085d93841251e225d8656683d3493fc
ABDELLATIF ZAMA <111080688+ZAMA05@users.noreply.github.com>
2022-09-22 19:11:26 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "isupper.h"
 
 /**
- * _isupper - checks is a character is uppercase
+ * isupper - checks is a character is uppercase
  * @c: carrier integer variable
  * Return: 1 if true, 0 if false
  */
 
-int isupper(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
